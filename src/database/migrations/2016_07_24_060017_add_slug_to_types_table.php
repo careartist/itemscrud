9477f09c9d9e30a7f93f1cd90d9f28d9558ab084
careartist <jingaandjinga@gmail.com>
2017-07-04 22:59:41 +0300

    update

--- src/database/migrations/2016_07_24_060017_add_slug_to_types_table.php
+++ src/database/migrations/2016_07_24_060017_add_slug_to_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddSlugToCategoriesTable extends Migration
+class AddSlugToTypesTable extends Migration
 {
     /**
      * Run the migrations.
